102455cb07a6bd3f87ff10799fb4965d91f3b1f8
William Belle <william.belle@gmail.com>
2017-02-05 13:54:17 +0100

    implement option functions

---
+++ src/js/options.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function saveOptions() {
+  var color = document.getElementById('color').value;
+  chrome.storage.local.set({
+    favoriteColor: color,
+  }, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+function restoreOptions() {
+  chrome.storage.local.get({
+    favoriteColor: 'green',
+  }, function(items) {
+    document.getElementById('color').value = items.favoriteColor;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
