965204f88c758a09f5f956689605ad65bed9d0ff
William Belle <william.belle@gmail.com>
2017-03-05 19:52:07 +0100

    add color green

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var github    = ['#eeeeee', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
+
 var blue      = ['#eeeeee', '#bbdefb', '#64b5f6', '#1e88e5', '#0d47a1'];
-var green     = ['#eeeeee', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
+var green     = ['#eeeeee', '#c8e6c9', '#81c784', '#43a047', '#1b5e20'];
 var halloween = ['#eeeeee', '#fdf156', '#ffc722', '#ff9711', '#04001b'];
 var orange    = ['#eeeeee', '#ffe0b2', '#ffb74d', '#fb8c00', '#e65100'];
 var pink      = ['#eeeeee', '#f8bbd0', '#f06292', '#e91e63', '#880e4f'];
 var red       = ['#eeeeee', '#ffcdd2', '#e57373', '#e53935', '#b71c1c'];
 
 var colors = {
+  github:    github,
   blue:      blue,
   green:     green,
   halloween: halloween,
@@ -21,8 +24,8 @@ function applyColorToRects(color) {
 
   for (var i = 0, l = rects.length; i < l; i++) {
     var fill = rects[i].getAttribute('fill');
-    for (var x = 0, y = green.length; x < y; x++) {
-      if (fill === green[x]) {
+    for (var x = 0, y = github.length; x < y; x++) {
+      if (fill === github[x]) {
         rects[i].style.fill = color[x];
       }
     }
