e3a67c6d8eb172f78e803726a4a7a62a6cfbd3e6
William Belle <william.belle@gmail.com>
2017-03-06 20:01:03 +0100

    add color deep purple

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -2,23 +2,25 @@
 
 var github    = ['#eeeeee', '#c6e48b', '#7bc96f', '#239a3b', '#196127'];
 
-var blue      = ['#eeeeee', '#bbdefb', '#64b5f6', '#1e88e5', '#0d47a1'];
-var green     = ['#eeeeee', '#c8e6c9', '#81c784', '#43a047', '#1b5e20'];
-var halloween = ['#eeeeee', '#fdf156', '#ffc722', '#ff9711', '#04001b'];
-var orange    = ['#eeeeee', '#ffe0b2', '#ffb74d', '#fb8c00', '#e65100'];
-var pink      = ['#eeeeee', '#f8bbd0', '#f06292', '#e91e63', '#880e4f'];
-var purple    = ['#eeeeee', '#e1bee7', '#ba68c8', '#8e24aa', '#4a148c'];
-var red       = ['#eeeeee', '#ffcdd2', '#e57373', '#e53935', '#b71c1c'];
+var blue       = ['#eeeeee', '#bbdefb', '#64b5f6', '#1e88e5', '#0d47a1'];
+var deeppurple = ['#eeeeee', '#d1c4e9', '#9575cd', '#5e35b1', '#311b92'];
+var green      = ['#eeeeee', '#c8e6c9', '#81c784', '#43a047', '#1b5e20'];
+var halloween  = ['#eeeeee', '#fdf156', '#ffc722', '#ff9711', '#04001b'];
+var orange     = ['#eeeeee', '#ffe0b2', '#ffb74d', '#fb8c00', '#e65100'];
+var pink       = ['#eeeeee', '#f8bbd0', '#f06292', '#e91e63', '#880e4f'];
+var purple     = ['#eeeeee', '#e1bee7', '#ba68c8', '#8e24aa', '#4a148c'];
+var red        = ['#eeeeee', '#ffcdd2', '#e57373', '#e53935', '#b71c1c'];
 
 var colors = {
-  github:    github,
-  blue:      blue,
-  green:     green,
-  halloween: halloween,
-  orange:    orange,
-  pink:      pink,
-  purple:    purple,
-  red:       red,
+  github:     github,
+  deeppurple: deeppurple,
+  blue:       blue,
+  green:      green,
+  halloween:  halloween,
+  orange:     orange,
+  pink:       pink,
+  purple:     purple,
+  red:        red,
 };
 
 function applyColorToRects(color) {
