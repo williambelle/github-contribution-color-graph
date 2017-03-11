90433cf534518049c1eaecc19c38fab1ead2b89a
William Belle <william.belle@gmail.com>
2017-03-11 22:26:12 +0100

    add color light green

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -11,6 +11,7 @@ var deeppurple  = ['#eeeeee', '#d1c4e9', '#9575cd', '#5e35b1', '#311b92'];
 var green       = ['#eeeeee', '#c8e6c9', '#81c784', '#43a047', '#1b5e20'];
 var indigo      = ['#eeeeee', '#c5cae9', '#7986cb', '#3949ab', '#1a237e'];
 var lightblue   = ['#eeeeee', '#b3e5fc', '#4fc3f7', '#039be5', '#01579b'];
+var lightgreen  = ['#eeeeee', '#dcedc8', '#aed581', '#7cb342', '#33691e'];
 var orange      = ['#eeeeee', '#ffe0b2', '#ffb74d', '#fb8c00', '#e65100'];
 var pink        = ['#eeeeee', '#f8bbd0', '#f06292', '#e91e63', '#880e4f'];
 var purple      = ['#eeeeee', '#e1bee7', '#ba68c8', '#8e24aa', '#4a148c'];
@@ -32,6 +33,7 @@ var colors = {
   green:       green,
   indigo:      indigo,
   lightblue:   lightblue,
+  lightgreen:  lightgreen,
   orange:      orange,
   pink:        pink,
   purple:      purple,
